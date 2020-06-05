77e8c9953e2de63d5a460c57b127bc47349c1757
Jurugo Felix <jurugofelix123@live.com>
2020-06-05 16:08:49 +0300

    Add project files.

---
+++ ConsoleApplication1PP/ConsoleApplication1PP.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+int main()
+{
+	std::cout << "calculating volume in square feet!!!" << std::endl;
+	std::cout << "input the length..." << std::endl;
+	int length;
+	std::cin >> length;
+	std::cout << "input the width: " << std::endl;
+	int width;
+	std::cin >> width;
+	std::cout << "input the height: " << std::endl;
+	int height;
+	std::cin >> height;
+	int volume = length * width * height;
+	std::cout << "the volume of ccontainer of length, " << length << " and width " << width << " and height " << height << " is " <<volume << std::endl;
+
+	return 0;
+}
